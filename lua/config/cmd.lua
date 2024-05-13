db895b4ddb162a5b2cbbba92da0eb56a571d74b3
Maxdep_ <danielsvacina8@gmail.com>
2024-05-13 14:43:05 +0100

    Disable auto comment on new line

--- lua/config/cmd.lua
+++ lua/config/cmd.lua
@@ -23,3 +23,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 500 })
     end,
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+    callback = function()
+        vim.opt.formatoptions:remove({ 'c', 'r', 'o' })
+    end,
+    desc = 'Disable New Line Comment',
+})
