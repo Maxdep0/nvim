088f03fa42471d0cf4e69df70ef2d71f6c162fbe
Maxdep_ <danielsvacina8@gmail.com>
2024-05-14 12:42:31 +0100

    Add border for lsp hover and signature help

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,6 +48,14 @@ return {
             vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
         end
 
+        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+            border = 'rounded',
+        })
+
+        vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+            border = 'rounded',
+        })
+
         vim.diagnostic.config({
             update_in_insert = true,
             virtual_text = {
