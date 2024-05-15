a431438885e80109439f7a75f33a2cf8dbfccba0
Maxdep_ <danielsvacina8@gmail.com>
2024-05-15 13:41:23 +0100

    Add highlight on cursor hold

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,6 +33,23 @@ return {
                     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
                 end, '[T]oggle Inlay [H]ints')
             end
+
+            if client.server_capabilities.documentHighlightProvider then
+                vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true })
+                vim.api.nvim_clear_autocmds({ buffer = bufnr, group = 'lsp_document_highlight' })
+                vim.api.nvim_create_autocmd('CursorHold', {
+                    callback = vim.lsp.buf.document_highlight,
+                    buffer = bufnr,
+                    group = 'lsp_document_highlight',
+                    desc = 'Document Highlight',
+                })
+                vim.api.nvim_create_autocmd('CursorMoved', {
+                    callback = vim.lsp.buf.clear_references,
+                    buffer = bufnr,
+                    group = 'lsp_document_highlight',
+                    desc = 'Clear All the References',
+                })
+            end
         end
 
         local signs = { Error = '● ', Warn = '● ', Hint = '● ', Info = '● ' }
