a56ea0632d8144a69401fb8e2597547b0229cbab
Maxdep_ <danielsvacina8@gmail.com>
2024-05-16 11:25:32 +0100

    Change keymaps for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -50,12 +50,33 @@ return {
 
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-k>'] = cmp.mapping.select_prev_item(),
                 ['<C-n>'] = cmp.mapping.select_next_item(),
+                ['<C-j>'] = cmp.mapping.select_next_item(),
+
                 ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-l>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-h>'] = cmp.mapping.scroll_docs(4),
+
                 ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<Esc>'] = cmp.mapping.abort(),
+
+                -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+                ['<CR>'] = cmp.mapping({
+                    i = function(fallback)
+                        if cmp.visible() and cmp.get_active_entry() then
+                            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                        else
+                            fallback()
+                        end
+                    end,
+                    s = cmp.mapping.confirm({ select = true }),
+                    -- c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                }),
+
+                ['<C-Space>'] = cmp.mapping.complete(),
 
                 ['<Tab>'] = cmp.mapping(function(fallback)
                     if luasnip.locally_jumpable(1) then
