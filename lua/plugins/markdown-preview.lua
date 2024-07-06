6045c5123e78a3764712086003263b0c1cfa687b
max <143852415+Maxdep0@users.noreply.github.com>
2024-07-06 04:20:07 +0100

    Change markdown preview installation

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -4,20 +4,18 @@ return {
         'iamcco/markdown-preview.nvim',
         cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
         ft = { 'markdown' },
-        cond = false,
         build = function()
             vim.fn['mkdp#util#install']()
         end,
     },
-    {
-        -- install with yarn or npm
-        'iamcco/markdown-preview.nvim',
-        cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-        build = 'cd app && yarn install',
-        cond = false,
-        init = function()
-            vim.g.mkdp_filetypes = { 'markdown' }
-        end,
-        ft = { 'markdown' },
-    },
+    -- {
+    --     -- install with yarn or npm
+    --     'iamcco/markdown-preview.nvim',
+    --     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    --     build = 'cd app && yarn install',
+    --     init = function()
+    --         vim.g.mkdp_filetypes = { 'markdown' }
+    --     end,
+    --     ft = { 'markdown' },
+    -- },
 }
