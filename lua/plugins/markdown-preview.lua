61da5fe109ac9a2ee72a96986c52c8d19481f5c8
max <143852415+Maxdep0@users.noreply.github.com>
2024-07-06 14:19:35 +0100

    Disabled auto markdown installation

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -4,6 +4,7 @@ return {
         'iamcco/markdown-preview.nvim',
         cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
         ft = { 'markdown' },
+        cond = false,
         build = function()
             vim.fn['mkdp#util#install']()
         end,
