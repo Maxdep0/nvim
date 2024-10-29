d4e1f2cf8a8ff17b1d67f45aaba18786113d4be9
Maxdep0 <143852415+Maxdep0@users.noreply.github.com>
2024-10-29 22:52:33 +0000

    .

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -53,13 +53,7 @@ return {
             },
             search = {
                 command = 'rg',
-                args = {
-                    '--color=never',
-                    '--no-heading',
-                    '--with-filename',
-                    '--line-number',
-                    '--column',
-                },
+                args = { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column' },
                 -- regex that will be used to match keywords.
                 -- don't replace the (KEYWORDS) placeholder
                 pattern = [[\b(KEYWORDS):]], -- ripgrep regex
