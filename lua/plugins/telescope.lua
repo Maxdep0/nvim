a1af071daf03d0d6c914d44c056b05ce884ff326
Maxdep0 <143852415+Maxdep0@users.noreply.github.com>
2025-06-07 03:44:31 +0100

    Add cond condition for fzf-native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,10 @@ return {
             'nvim-telescope/telescope-fzf-native.nvim',
 
             build = 'make',
+
+            cond = function()
+                return vim.fn.executable('make') == 1
+            end,
         },
         {
             'nvim-tree/nvim-web-devicons',
