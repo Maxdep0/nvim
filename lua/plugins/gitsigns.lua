89d7c2a4ee1d9b7dd86f7ce3407b8e949dbd8e8a
Maxdep0 <143852415+Maxdep0@users.noreply.github.com>
2025-06-14 21:14:24 +0100

    Remove gitsigns binds
    Add custom telescope cmd menu

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -19,37 +19,6 @@ return {
             current_line_blame = true,
             max_file_length = 20000,
 
-            on_attach = function(bufnr)
-                local gs = require('gitsigns')
-
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, '<leader>'.. l, r, opts)
-                end
-
-                -- Navigation
-                map('n', ']c', function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ ']c', bang = true })
-                    else
-                        gs.nav_hunk('next')
-                    end
-                end, { desc = 'Git: Navigate to the next git hunk' })
-
-                map('n', '[c', function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ '[c', bang = true })
-                    else
-                        gs.nav_hunk('prev')
-                    end
-                end, { desc = 'Git: Navigate to the previous git hunk' })
-
-                map({'n', 'v'}, '<F9>', ':Gitsigns stage_hunk<CR>', { desc = 'Git: [H]unt [S]tage for commit' })
-                map('n',        '<F10>', gs.stage_buffer,           { desc = 'Git: [H]unt [S]tage entire file' })
-                map('n',        '<F11>', gs.preview_hunk,           { desc = 'Git: [T]oggle [D]elete preview window showing current hunk' })
-                map('n',        '<F12>', gs.toggle_deleted,         { desc = 'Git: [T]oggle [D]iff view of deleted lines in current file' })
-            end,
         })
     end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,32 @@
--------------- stylua: ignore
+-- stylua: ignore
+local cmds = {
+    {
+        name = 'Preview Hunk I',
+        fn = function() vim.cmd('Gitsigns preview_hunk_inline') end,
+        info = 'Show preview hunks inline',
+    },
+    {
+        name = 'Preview Hunk P',
+        fn = function() vim.cmd('Gitsigns preview_hunk') end,
+        info = 'Show preview hunks in popup',
+    },
+    {
+        name = 'Stage Hunk',
+        fn = function() vim.cmd('Gitsigns stage_hunk') end,
+        info = 'Stage current hunk',
+    },
+    {
+        name = 'Undo Stage Hunk',
+        fn = function() vim.cmd('Gitsigns undo_stage_hunk') end,
+        info = 'Unstage current hunk',
+    },
+    {
+        name = 'Stage Buffer',
+        fn = function() vim.cmd('Gitsigns stage_buffer') end,
+        info = 'Stage current buffer',
+    },
+}
+
 return {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
@@ -14,107 +42,109 @@ return {
                 return vim.fn.executable('make') == 1
             end,
         },
-        {
-            'nvim-tree/nvim-web-devicons',
-            enabled = vim.g.have_nerd_font,
-        },
     },
+
     config = function()
         local builtin = require('telescope.builtin')
         local actions = require('telescope.actions')
+        local pickers = require('telescope.pickers')
+        local finders = require('telescope.finders')
         local action_layout = require('telescope.actions.layout')
+        local actions_state = require('telescope.actions.state')
 
         require('telescope').setup({
             defaults = {
-                path_display = { 'smart' },
-
-                file_ignore_patterns = { '.git/', 'node_modules', 'dist', 'build', 'target', '__pycache__', 'venv' },
-
                 mappings = {
                     i = {
                         ['<esc>'] = actions.close,
-                        -- ['<C-n>'] = actions.cycle_history_next,
-                        -- ['<C-p>'] = actions.cycle_history_prev,
-
                         ['<C-j>'] = actions.move_selection_next,
                         ['<C-k>'] = actions.move_selection_previous,
-                        --
-                        -- ['<C-c>'] = actions.close,
-                        --
-                        -- ['<Down>'] = actions.move_selection_next,
-                        -- ['<Up>'] = actions.move_selection_previous,
-                        --
                         ['<C-l>'] = actions.select_default,
-                        -- ['<C-x>'] = actions.select_horizontal,
-                        -- ['<C-v>'] = actions.select_vertical,
-                        -- ['<C-t>'] = false, --actions.select_tab,
-                        --
-                        -- ['<C-u>'] = actions.preview_scrolling_up,
-                        -- ['<C-d>'] = actions.preview_scrolling_down,
-                        --
-                        -- ['<PageUp>'] = actions.results_scrolling_up,
-                        -- ['<PageDown>'] = actions.results_scrolling_down,
-                        --
-                        -- ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
-                        -- ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
-                        -- ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
-                        -- ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-                        -- ['<C-l>'] = false,-- actions.complete_tag,
                         ['<C-_>'] = actions.which_key,
                         ['<M-p>'] = action_layout.toggle_preview,
                     },
                 },
             },
-            extensions = {
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                    case_mode = 'smart_case',
-                },
-            },
+
             pickers = {
                 find_files = { previewer = false, theme = 'dropdown' },
-                live_grep = { previewer = false, theme = 'dropdown' },
                 grep_string = { previewer = false, theme = 'dropdown' },
-                buffers = { previewer = false, theme = 'dropdown' },
-                git_files = { previewer = false, theme = 'dropdown' },
-                lsp_references = { previewer = false, theme = 'dropdown', layout_config = { width = 0.6 } },
-                diagnostics = { previewer = false, theme = 'dropdown', layout_config = { width = 0.6 } },
+                live_grep = { previewer = false, theme = 'dropdown' },
+                diagnostics = { previewer = false, theme = 'dropdown' },
+                lsp_references = { previewer = false, theme = 'dropdown' },
                 help_tags = { previewer = false, theme = 'dropdown' },
-                keymaps = { previewer = false, theme = 'dropdown', layout_config = { width = 0.6 } },
-                lsp_document_symbols = { previewer = false, theme = 'dropdown', layout_config = { width = 0.6 } },
+                keymaps = { previewer = false, theme = 'dropdown' },
             },
+
+            extensions = { fzf = {} },
         })
 
-        local ok = pcall(require('telescope').load_extension, 'fzf')
+        require('telescope').load_extension('fzf')
 
-        if not ok then
+        local fzf_ok = pcall(require('telescope').load_extension, 'fzf')
+
+        if not fzf_ok then
             print('Failed to load telescope-fzf-native.nvim')
         end
+
+        -- INFO: CUSTOMS
+
+        local entry_display = require('telescope.pickers.entry_display').create({
+            separator = ' │ ',
+            items = { { width = 24 }, {}, { remaining = true } },
+        })
+
+        local function custom_menu()
+            pickers
+                .new(require('telescope.themes').get_dropdown({}), {
+                    prompt_title = 'Custom Actions',
+                    finder = finders.new_table({
+                        results = cmds,
+                        entry_maker = function(entry)
+                            return {
+                                value = entry,
+                                display = function()
+                                    return entry_display({ entry.name, entry.info or '' })
+                                end,
+                                ordinal = entry.name .. (entry.info or ''),
+                            }
+                        end,
+                    }),
+                    attach_mappings = function(prompt_bufnr, map1)
+                        local select = function()
+                            actions.close(prompt_bufnr)
+                            actions_state.get_selected_entry().value.fn()
+                        end
+                        map1('i', '<CR>', select)
+                        map1('n', '<CR>', select)
+                        return true
+                    end,
+                })
+                :find()
+        end
+
+        -- INFO: Keymaps
 
         local map = function(keys, func, desc)
             vim.keymap.set('n', keys, func, { desc = 'Search: ' .. desc })
         end
 
-        map('<leader><space>', builtin.find_files, 'Search Files')
-
-        map('<leader>sw', builtin.grep_string, 'Search current word')
+        map('<leader><space>', builtin.find_files, 'Search files')
+        map('<leader>sw', builtin.grep_string, 'Search word under the cursor')
         map('<leader>sg', builtin.live_grep, 'Search by grep')
-        map('<leader>sb', builtin.buffers, 'Search for existing buffers')
-        map('<leader>sp', function()
-            local project_root = vim.fs.dirname(vim.fs.find('.git', { upward = true })[1]) or vim.fn.getcwd()
-            builtin.find_files({ no_ignore = true, hidden = true, cwd = project_root })
-        end, 'Search project files from project root')
-
-        map('<leader>sr', builtin.lsp_references, 'Search LSP references')
         map('<leader>sd', builtin.diagnostics, 'Search diagnostics')
+        map('<leader>sr', builtin.lsp_references, 'Search LSP references')
         map('<leader>sh', builtin.help_tags, 'Search help')
         map('<leader>sk', builtin.keymaps, 'Search keymaps')
-        map('<leader>ss', builtin.lsp_document_symbols, 'Search document symbols')
+        map('<leader>s', custom_menu, 'Search commands')
 
         map('<leader>sn', function()
             builtin.find_files({ cwd = vim.fn.stdpath('config') })
-        end, 'Search neovim files')
+        end, 'Search nvim dir')
+
+        map('<leader>sp', function()
+            local project_root = vim.fs.dirname(vim.fs.find('.git', { upward = true })[1]) or vim.fn.getcwd()
+            builtin.find_files({ no_ignore = true, hidden = true, cwd = project_root })
+        end, 'Search git project from root')
     end,
 }
