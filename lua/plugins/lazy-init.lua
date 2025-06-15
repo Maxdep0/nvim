78dac3b34148471bc567744edb7890a134478b41
Maxdep0 <143852415+Maxdep0@users.noreply.github.com>
2025-06-15 01:24:54 +0100

    Remove/Merge files

--- lua/plugins/lazy-init.lua
+++ lua/plugins/lazy-init.lua
@@ -3,14 +3,19 @@ return {
     {
         'windwp/nvim-autopairs',
         event = 'InsertEnter',
-        config = function()
-            require('nvim-autopairs').setup({})
-        end,
+        config = function() require('nvim-autopairs').setup({}) end,
     },
     {
         'mbbill/undotree',
         config = function()
             vim.keymap.set('n', '<leader>U', vim.cmd.UndotreeToggle, { desc = 'Keymap: Toggle Undo Tree' })
         end,
+    },
+    {
+        'iamcco/markdown-preview.nvim',
+        cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+        ft = { 'markdown' },
+        cond = false,
+        build = function() vim.fn['mkdp#util#install']() end,
     },
 }
